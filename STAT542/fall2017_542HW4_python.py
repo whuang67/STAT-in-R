27d652c8f1be1b16c2cc90233176f720a4d170dc
Wenke Huang <whuang67@illinois.edu>
2017-11-25 19:33:54 -0600

    Update fall2017_542HW4_python.py

--- STAT542/fall2017_542HW4_python.py
+++ STAT542/fall2017_542HW4_python.py
@@ -1,163 +1,180 @@
-###### Question 1 #######################################################
-#########################################################################
-#########################################################################
-###### a ################################################################
-NadarayaWatson <- function(x, y, test=x, bandwidth=1){
-  Gaussian_Kernal <- function(x1, x2) 1/sqrt(2*pi) * exp(-sum(((x1-x2)/bandwidth)^2))
-  
-  EachStep <- function(test){
-    K = apply(x, MARGIN=1, FUN=Gaussian_Kernal, test)
-    return(sum(K*y) / sum(K))
-  }
-  
-  output <- apply(x, MARGIN=1, FUN=EachStep)
-  return(output) 
-}
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 13:09:06 2017
 
-get_RMSE <- function(y_real, y_pred) (mean((y_real-y_pred)^2))^.5
+@author: whuang67
+"""
 
-P = 20; N = 200
-X = as.matrix(mvrnorm(N, mu=rep(0,P), Sigma=diag(P)))
-y = apply(X, 1, sum) + rnorm(N)
+import numpy as np
 
-X_test = as.matrix(mvrnorm(40, mu=rep(0,P), Sigma=diag(P)))
-y_test = apply(X_test, 1, sum) + rnorm(40)
+############################################
+### Question 1 #############################
+############################################
+###### a ###################################
+def get_RMSE(y, y_pred): return (np.mean((y-y_pred)**2))**.5
 
-get_RMSE(y, NadarayaWatson(X, y))
-get_RMSE(y_test, NadarayaWatson(X, y, X_test))
-
-###### b ################################################################
-dat = read.csv("C:/users/whuang67/downloads/Video_Games_Sales_as_at_22_Dec_2016.csv")
-y = log(dat$Global_Sales + 1)
-X = subset(dat, select = c("Critic_Score", "Critic_Count", "User_Score"))
-X$User_Score = as.numeric(as.character(X$User_Score))
-dat1 = na.omit(data.frame(y, X))
-
-
-###### Question 2 #######################################################
-#########################################################################
-#########################################################################
-###### a ################################################################
-library(MASS)
-set.seed(1)
-P = 20; N = 200
-X = as.matrix(mvrnorm(N, mu=rep(0,P), Sigma=diag(P)))
-
-beta = c(.5, .5, .5, .5)
-# Y = 1 + X[, 1:4] %*% beta + rnorm(N)
-
-library(randomForest)
-mtry_ = c(2, 3, 4, 5); nodesize_ = c(1, 2, 3, 4)
-Y_real = array(0, dim=c(N, 20)); Y_pred = array(0, dim=c(N, 20))
-cov_matrix = array(0, c(4, 4))
-for(i in 1:4){
-  for(j in 1:4){
-    for(k in 1:20){
-      Y_real[, k] = 1 + X[, 1:4] %*% beta + rnorm(N)
-      dat = data.frame(y=Y_real[, k], X)
-      model <- randomForest(y ~ ., data = dat,
-                            ntree = 10,
-                            mtry=mtry_[i],
-                            nodesize=nodesize_[j])
-      Y_pred[, k] = predict(model, newdata = dat, type="response")
-    }
-    cov_matrix[i, j] = sum(diag(cov(t(Y_real), t(Y_pred))))
-  }
-}
-cov_matrix
-
-Y_real = array(0, dim=c(N, 20)); Y_pred = array(0, dim=c(N, 20))
-for(k in 1:20){
-  Y_real[, k] = 1 + X[, 1:4] %*% beta + rnorm(N)
-  model = randomForest(X, Y_real[, k])
-  Y_pred[, k] = predict(model, newdata = X)
-}
-
-sum(diag(cov(t(Y_real), t(Y_pred))))
-
-###### b ################################################################
-
-
-###### Question 3 #######################################################
-#########################################################################
-#########################################################################
-###### a ################################################################
-stump <- function(x, y, w = rep(1, length(x))/length(x)){
-  ordered_x <- sort(x); max_score = NA; cut = NA
-  
-  for(i in ordered_x[-length(ordered_x)]){
-    x_L <- x[x<=i]; y_L <- y[x<=i]; w_L <- w[x<=i]
-    x_R <- x[x>i];  y_R <- y[x>i];  w_R <- w[x>i]
+class NadarayaWatsonRegressor:
+    def __init__(self, train_X, train_y, test_X, bandwidth=1):
+        self.train_X = train_X
+        self.train_y = train_y
+        self.test_X = test_X
+        self.bandwidth = bandwidth
+        
+    def Gaussian_kernal(self, x1, x2):
+        return np.exp(-np.sum(((x1-x2)/self.bandwidth)**2)) / (2*np.pi)**.5
     
-    p_L <- sum(w_L*(y_L==1))/sum(w_L); p_R <- sum(w_R*(y_R==1))/sum(w_R)
+    def EachStep(self, test):
+        K = np.apply_along_axis(self.Gaussian_kernal, 1, self.train_X, test)
+        return (K*self.train_y).sum()/K.sum()
     
-    Gini_L <- p_L*(1-p_L); Gini_R <- p_R*(1-p_R)
+    def Get_output(self):
+        return np.apply_along_axis(self.EachStep, 1, self.test_X)
+
+
+n = 200; p = 20
+## Training set
+X = np.random.multivariate_normal(np.zeros(p), np.identity(p), n)
+y = np.sum(X, axis = 1) + np.random.normal(size=n)
+## Testing set
+X_test = np.random.multivariate_normal(np.zeros(p), np.identity(p), 40)
+y_test = np.sum(X_test, axis = 1) + np.random.normal(size=40)
+## Predictions
+y_pred = NadarayaWatsonRegressor(X, y, X).Get_output()
+y_pred_test = NadarayaWatsonRegressor(X, y, X_test).Get_output()
+## Performances
+print(get_RMSE(y, y_pred))
+print(get_RMSE(y_test, y_pred_test))
+
+
+###### b ###################################
+import os
+os.chdir("C:/users/whuang67/downloads")
+
+import pandas as pd
+dat = pd.read_csv("Video_Games_Sales_as_at_22_Dec_2016.csv")
+dat = dat[["Global_Sales", "Critic_Score", "Critic_Count", "User_Score"]]
+for name in ["Global_Sales", "Critic_Score", "Critic_Count", "User_Score"]:
+    dat[name] = pd.to_numeric(dat[name], errors = "coerce")
+dat.dropna(axis=0, inplace=True)
+
+y = np.log(1+dat["Global_Sales"].values)
+X = dat[["Critic_Score", "Critic_Count", "User_Score"]].values
+
+
+from sklearn.model_selection import KFold
+kf = KFold(n_splits = 10); errors = np.array([]); cv_errors = np.array([])
+for bandwidth in [1,2,3,4,5]:
+    for train_index, test_index in kf.split(X):
+        train_X, train_y = X[train_index], y[train_index]
+        test_X, test_y = X[test_index], y[test_index]
+        pred_y = NadarayaWatsonRegressor(train_X, train_y, test_X, bandwidth).Get_output()
+        errors = np.append(errors, get_RMSE(test_y, pred_y))
+    cv_errors = np.append(cv_errors, errors.mean())
+
+import matplotlib.pyplot as plt
+plt.plot(np.arange(5), cv_errors); plt.show()
+
+
+
+############################################
+### Question 2 #############################
+############################################
+###### a ###################################
+from sklearn.ensemble import RandomForestRegressor
+n = 200; p = 20
+X = np.random.multivariate_normal(np.zeros(p), np.identity(p), n)
+# y = 1 + np.matmul(X[:, 0:4], np.array([0.5, 0.5, 0.5, 0.5])) + np.random.normal(size=n)
+
+y_real = np.zeros(shape = (n, p)); y_pred = np.zeros(shape = (n, p))
+cov_matrix = np.zeros(shape=(4, 4))
+mtry = [2, 3, 4, 5]; nodesize = [1, 2, 3, 4]
+for i in range(4):
+    for j in range(4):
+        for k in range(20):
+            y_real[:, k] = 1 + np.matmul(X[:, 0:4],
+                  np.array([0.5, 0.5, 0.5, 0.5])) + np.random.normal(size=n)
+            reg = RandomForestRegressor(
+                    n_estimators = 10,
+                    min_samples_leaf = mtry[i],
+                    max_features = nodesize[j]).fit(X, y_real[:, k])
+            y_pred[:, k] = reg.predict(X)
+        cov_matrix[i, j] = np.cov(y_real, y_pred)[n:2*n-1, 0:n-1].trace()
+
+print(cov_matrix)
+
+###### b ###################################
+for i in [10, 50, 100, 200, 300, 500]:
+    y_real = np.sum(X, axis=1) + np.random.normal(size=n)
+    reg = RandomForestRegressor(n_estimators=i).fit(X, y_real)
+    y_pred = reg.predict(X)
+    print(np.std(y_pred))
+
+
+
+############################################
+### Question 3 #############################
+############################################
+###### a ###################################
+def CART(X, y, W=None):
+    if W is None:
+        W = np.ones(X.shape[0])/X.shape[0]
+    def get_score_(cut, y, W):
+        left_idx = (X <= cut)
+        y_L = y[left_idx]; y_R = y[~left_idx]
+        W_L = W[left_idx]; W_R = W[~left_idx]
+        p_L = W_L[y_L == 1].sum()/W_L.sum()
+        p_R = W_R[y_R == 1].sum()/W_R.sum()
+        
+        Gini_L = p_L*(1-p_L); Gini_R = p_R*(1-p_R)
+        return (W_L.sum()*Gini_L + W_R.sum()*Gini_R)/W.sum()
     
-    score <- - (sum(w_L)*Gini_L + sum(w_R)*Gini_R)/sum(w)
+    possible_cuts = np.sort(X)[:-1]
+    output = np.array([get_score_(cut, y, W) for cut in possible_cuts])
+    best_cut = possible_cuts[np.argmin(output)]
+    prediction = np.where(X <= best_cut, 1, -1)
+    # apply_along_axis(get_score_, 0, possible_cuts, y, W)
+    return {"best_cut": best_cut, "prediction": prediction}
+
+
+X = np.hstack((np.random.uniform(0, 3, 50), np.random.uniform(2, 5, 50)))
+y = np.hstack((np.ones(50), -np.ones(50)))
+W = np.ones(100)/100
+
+cut, pred = CART(X, y, W).values()
+
+plt.scatter(X, y); plt.axvline(x=cut); plt.show()
+
+
+###### b ###################################
+def Adaboost(X, y, steps = 100, bootstrap = True):
+    weights = np.ones(X.shape[0])/X.shape[0]
+    final_predict = np.zeros(y.shape[0])
     
-    if(score > max_score | is.na(max_score)){
-      max_score = score
-      cut = i
-    }
-    
-  y_predict <- ifelse(x<=cut, 1, -1)
-  }
-  return(list(cut = cut,
-              predict = y_predict))
-}
+    for i in range(steps):
+        if bootstrap is True:
+            ### Bootstrap Sampling ###
+            idx = np.arange(X.shape[0])
+            bootstrap_idx = np.unique(np.random.choice(idx, idx.shape[0]))
+            X_, y_, weights_ = X[bootstrap_idx], y[bootstrap_idx], weights[bootstrap_idx]
+            cut = CART(X_, y_, weights_)["best_cut"]
+            predict = np.where(X <= cut, 1, -1)
+            ##########################
+        else:
+            predict = CART(X, y, weights)["prediction"]
+            
+        epsilon = np.dot(weights, (predict!=y)*1)
+        alpha = .5*np.log((1-epsilon)/epsilon)
+        w_ = weights*np.exp(-alpha * y * predict)
+        weights = w_/np.sum(w_)
+        
+        final_predict += alpha*predict
+    return np.sign(final_predict)
 
+np.random.seed(1)
+n = 300; X = np.random.uniform(size=n)
+y = 2*(np.random.binomial(n=1, p=(np.sin(4*np.pi*X)+1)/2, size=n)-0.5)
 
-### Test
-set.seed(1)
-x = c(rnorm(100, mean = 1, sd = 1), rnorm(100, mean = 3, sd = 1))
-y = c(rep(1,100), rep(-1,100))
-w = rep(1,length(x))/length(x)
-result <- stump(x, y, w)
-
-## install.packages("ggplot2")
-library(ggplot2)
-ggplot() + geom_point(mapping = aes(x = x, y = y)) + geom_vline(xintercept=result$cut, color="red")
-
-###### b ################################################################
-Adaboost <- function(x, y, step = 1000){
-  w = rep(1, length(x))/length(x)
-  alpha = rep(NA, step); predict = array(NA, dim=c(length(y), step))
-  
-  for(i in 1:step){
-
-    ## Bootstrap #################################################
-    idx <- unique(sample(1:length(x), length(x), replace=TRUE))
-    x_ <- x[idx]; y_ <- y[idx]; w_ <- w[idx]
-    ##############################################################
-    result <- stump(x_, y_, w_)
-
-    cutoff <- result$cut
-    predict[, i] = ifelse(x <= cutoff, 1, -1)
-    error_t <- sum((predict[, i] != y)*w)/sum(w)
-    
-    alpha[i] <- 1/2 * log((1-error_t)/error_t)
-    w_ <- w * exp(- alpha[i] * y*predict[, i])
-    w <- w_/sum(w_)
-  }
-  
-  score = predict %*% alpha
-  final_pred <- ifelse(score > 0, 1, -1)
-  
-  return(final_pred)
-}
-
-
-### Test
-n = 300
-set.seed(1)
-x = runif(n)
-y = (rbinom(n , 1 , (sin(4*pi*x)+1)/2)-0.5)*2
-ggplot(mapping = aes(x = x, y = y)) + geom_point()
-
-#### Single CART
-result <- stump(x, y)
-mean(result$predict == y)
-
-#### Adaboost
-result <- Adaboost(x, y)
-mean(result == y)
+y_pred1 = CART(X, y)["prediction"]
+print(np.mean(y_pred1 == y))
+y_pred = Adaboost(X, y, steps = 1000, bootstrap=True)
+print(np.mean(y_pred == y))
